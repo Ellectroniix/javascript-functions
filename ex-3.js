6c007283c975671f82f0d60d143c64acf5074d45
JR <Jutarat.rng@gmail.com>
2026-09-21 19:22:21 +0700

    fix: ex-3

--- ex-3.js
+++ ex-3.js
@@ -1,1 +1,13 @@
 // Start coding here
+let add = (a,b) => a+b;
+let subtract = (a,b) => a-b;
+let multiply = (a,b) => a*b;
+let divide = (a,b) => a/b;
+let add10_5 = add(10,5);
+let subtract10_5 = subtract(10,5);
+let multiply10_5 = multiply(10,5);
+let divide10_5 = divide(10,5);
+console.log(add10_5);
+console.log(subtract10_5);
+console.log(multiply10_5);
+console.log(divide10_5);
