a8dcac71cfd766810a73e47a41ab1e49cbb1e9df
JR <Jutarat.rng@gmail.com>
2026-09-21 19:19:26 +0700

    fix: ex-2

--- ex-2.js
+++ ex-2.js
@@ -1,1 +1,25 @@
 // Start coding here
+let add = function(a,b){
+    const result = a+b;
+    return result;
+};
+let subtract = function(a,b){
+    const result = a-b;
+    return result;
+};
+let multiply = function(a,b){
+    const result = a*b;
+    return result;
+};
+let divide = function(a,b){
+    const result = a/b;
+    return result;
+};
+let add10_5 = add(10,5);
+let subtract10_5 = subtract(10,5);
+let multiply10_5 = multiply(10,5);
+let divide10_5 = divide(10,5);
+console.log(add10_5);
+console.log(subtract10_5);
+console.log(multiply10_5);
+console.log(divide10_5);
